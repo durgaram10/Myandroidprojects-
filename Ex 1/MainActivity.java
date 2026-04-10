fd6cad7c965eb47c16386e61dc14bfd6ba7228e3
durgaram10 <dr0209400@gmail.com>
2026-04-10 08:43:08 +0530

    Update

--- Ex 1/MainActivity.java
+++ Ex 1/MainActivity.java
@@ -1,1 +1,29 @@
+package com.example.Ex1;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button Clicked!");
+            }
+        });
+    }
+}
